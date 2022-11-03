7158d54df7341c1d2dd317728ab4aec141f1079a
eddysantiagoo <eddysantiagogh@gmail.com>
2022-11-02 19:52:10 -0500

    :bug: Solución bug nav-bar y media querys

--- LandingIUD/js/script.js
+++ LandingIUD/js/script.js
@@ -1,5 +1,36 @@
-const foot = document.getElemntByUd('year');
+const foot = document.getElemntById('year');
 
-const year = new Date().getFullYear
+const year = new Date().getFullYear();
 
 foot.innerHTML = year;
+
+const buttonMenu = document.getElemntById('btn-menu')
+const list = document.getElementById('nav-items')
+
+buttonMenu.addEventListener('click', () =>{
+    show = !show;
+    if (show) {
+        list.style.display = 'block'
+    }else{
+        list.style.display = 'none'
+    }
+})
+
+window.addEventListener('resize', () =>{
+    const width = screen.width;
+
+    if (width >= 425.999) {
+        list.style.display = 'flex'
+    }else{
+        list.style = 'none'
+        show = false;
+    }
+})
+
+(function(){
+    if (width >= 425.999) {
+        list.style.display = 'flex'
+    }else{
+        list.style = 'none'
+    }
+})();
